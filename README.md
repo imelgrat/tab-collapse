7c61a103ede8f1092ebcf154933c9f0b573f2d13
Iván Melgrati <imelgrat@yahoo.com.ar>
2017-12-27 18:34:32 -0300

    Initial upload
    This commit contains the first code release

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# tab-collapse
- While Bootstrap's Collapse component (accordions) can be very useful and works well in mobile websites, tab-based navigation usually works better in desktop screens. This component provides a way to integrate both tabs and accordions using some extra CSS and Javascript.
+# Tab/Collapse
+While Bootstrap's Collapse component (accordions) can be very useful and works well in mobile websites, tab-based navigation usually works better in desktop screens. This component provides a way to integrate both tabs and accordions using some extra CSS and Javascript.
+
+Developed by [Ivan Melgrati](https://imelgrat.me) [![Twitter](https://img.shields.io/twitter/url/https/github.com/imelgrat/tab-collapse.svg?style=social)](https://twitter.com/imelgrat)
+
+License: [![GitHub license](https://img.shields.io/github/license/imelgrat/tab-collapse.svg)](https://github.com/imelgrat/tab-collapse/blob/master/LICENSE)
+
+Status: [![GitHub release](https://img.shields.io/github/release/imelgrat/tab-collapse.svg)](https://github.com/imelgrat/tab-collapse) [![GitHub issues](https://img.shields.io/github/issues/imelgrat/tab-collapse.svg)](https://github.com/imelgrat/tab-collapse/issues) [![GitHub forks](https://img.shields.io/github/forks/imelgrat/tab-collapse.svg)](https://github.com/imelgrat/tab-collapse/network) [![GitHub stars](https://img.shields.io/github/stars/imelgrat/tab-collapse.svg)](https://github.com/imelgrat/tab-collapse/stargazers)
+
+
+Requirements
+------------
+
+*   jQuery >= 3.0
+*   Bootstrap >= 3.3
+
+Installation
+------------
+
+While most of the code required to make tabs and accordions work comes from Bootstrap, it is necessary to add some extra code to make the "component switch" between tabs and accordions view and navigation work.
+After adding both jQuery and Bootstrap, it's necessary to add the component's CSS rules and JavaScript code
+
+```html
+<link rel="stylesheet" href="../dist/css/tabcollapse-style.css" crossorigin="anonymous">
+<script src="../dist/js/tabcollapse-script.js" crossorigin="anonymous"></script>
+```
+
+Usage
+------------
+For the Tab/Collapse to work, it's necessary to create three blocks. The *vertab-container* div (the whole component's container, used as CSS and JS selector), the *vertab-menu* section (tab navigation links) and the *vertab-accordion* section (accordion and content sections). 
+For each content section, it's necessary to create a link inside the *list-group* block and a *vertab-content* block.
+
+```html
+<div class="col-lg-12 vertab-container">
+	<div class="col-lg-3 col-md-3 col-sm-3 vertab-menu">
+		<div class="list-group">
+			<a href="#" class="list-group-item text-left"> This is Tab 1 </a>
+			.......
+		</div>
+	</div>
+	<div id="accordion" class="col-lg-9 col-md-9 col-sm-9 col-xs-12 vertab-accordion panel-group">
+		<div class="vertab-content">
+			<div class="panel-heading">
+				<h4 class="panel-title" data-toggle="collapse" data-parent="#accordion" data-target="#collapse1"> This is Heading 1 </h4>
+			</div>
+			<div id="collapse1" class="panel-collapse collapse">
+				<div class="panel-body">
+					Here goes the content
+				</div>
+			</div>
+		</div>
+		......... 
+	</div>
+</div>
+```
+Feedback
+--------
+
+Please open an issue to request a feature or submit a bug report. Or even if
+you just want to provide some feedback, I'd love to hear. I'm also available on
+Twitter as [@imelgrat](https://twitter.com/imelgrat).
+
+Contributing
+------------
+
+1.  Fork it.
+2.  Create your feature branch (`git checkout -b my-new-feature`).
+3.  Commit your changes (`git commit -am 'Added some feature'`).
+4.  Push to the branch (`git push origin my-new-feature`).
+5.  Create a new Pull Request.
+
+Links
+====
+* Article/Tutorial: https://imelgrat.me/javascript/integrate-bootstrap-tabs-accordions/
+* Live example: https://codepen.io/imelgrat/details/wpGgpN/
---
+++ dist/js/tabcollapse-script.js
@@ -0,0 +1,128 @@
+// Screen-width breakpoint
+var tc_breakpoint = 767;
+
+jQuery(document).ready(function() 
+{
+	"use strict";
+	
+	// Switch tabs and update panels classes - Adjust container height
+    jQuery(".vertab-container .vertab-menu .list-group a").click(function(e) 
+	{
+        var index = jQuery(this).index();
+		var container = jQuery(this).parents('.vertab-container');
+		var accordion = container.find('.vertab-accordion');
+		var contents = accordion.find(".vertab-content");
+		
+		e.preventDefault();
+		
+        jQuery(this).addClass("active");
+        jQuery(this).siblings('a.active').removeClass("active");
+        
+		contents.removeClass("active");
+        contents.eq(index).addClass("active");
+		container.data('current',index);
+
+		//Adjust container height
+		jQuery(this).parents('.vertab-menu').css('min-height',jQuery(container).children('.vertab-accordion').height() + 20);
+    });
+	
+	// Collapse accordion panels (except the one the user just opened) and add "active" class to the panel heading 
+	jQuery('.vertab-accordion').on('show.bs.collapse','.collapse', function() 
+	{
+		var accordion, container, current, index;
+		
+		accordion = jQuery(this).parents('.vertab-accordion');
+		container = accordion.parents('.vertab-container');
+		
+		accordion.find('.collapse.in').each(function()
+		{
+			jQuery(this).collapse('hide');
+		});		
+		
+		jQuery(this).siblings('.panel-heading').addClass('active');
+		
+		current = accordion.find('.panel-heading.active');
+		index = accordion.find('.panel-heading').index(current);
+		
+		container.data('current',index);
+	});
+								   
+	// Remove "active" class from heading when collapsing the current panel 
+	jQuery('.vertab-accordion .panel-collapse').on('hide.bs.collapse', function () {
+		jQuery(this).siblings('.panel-heading').removeClass('active');
+	});	
+	
+	// Manage resize / rotation events
+	jQuery( window ).on( "resize orientationchange", function(  ) 
+	{
+		resize_vertical_accordions();
+	});
+	
+	// Scroll accordion to show the current panel
+	jQuery(".vertab-accordion .panel-heading").click(function () 
+	{
+		var el = this;
+		setTimeout(function(){jQuery("html, body").animate({scrollTop: jQuery(el).offset().top - 10 }, 1000);},500);
+		
+		return true;
+	});
+	
+	//Initial Panels setup
+	resize_vertical_accordions(  );
+});
+
+function resize_vertical_accordions(  ) 
+{
+	"use strict";
+	jQuery('.vertab-container').each(function(i, e)
+	{
+		var index, menu, contents; 
+		var container = jQuery(this);
+		
+		// Setup current tab/panel (default to first tab/panel)
+		index = jQuery(this).data('current');
+		if(index === undefined)
+		{
+			jQuery(this).data('index',0);
+			index = 0;
+		}
+		
+		// If using a desktop-size screen, manage as tabbed panels
+		if( jQuery( window ).width() > tc_breakpoint)
+		{
+			// Reset panels heights (Bootstrap's accordions sets heights to zero)
+			jQuery(this).find('.panel-collapse.collapse').css('height','auto');
+			
+			// Clean tab-navigation styles
+			menu = jQuery(this).find('.vertab-menu .list-group a');
+			menu.removeClass("active");
+
+			// Clean tab-panels styles
+			contents = jQuery(this).find(".vertab-accordion .vertab-content");
+			contents.removeClass("active");
+			
+			// Update tab navigation and panels styles
+			menu.eq(index).addClass('active');			
+			contents.eq(index).addClass("active");
+			
+			// Update tab navigation's height to match current tab
+			jQuery(this).children('.vertab-menu').css('min-height',jQuery(this).children('.vertab-accordion').height() + 20);			
+		}
+		else // If using a mobile device (phone + tablets), manage as accordion
+		{
+			// Close all panels
+			jQuery(this).find('.vertab-content .panel-collapse.collapse').collapse('hide');
+			
+			// Clean styles from headings
+			jQuery(this).find('.vertab-content .panel-heading').removeClass('active');
+			
+			// Wait until all panels have collapsed and mark the one the user selected as active.
+			setTimeout(function()
+			{
+				jQuery(container).find('.vertab-content .panel-heading').eq(index).addClass("active");
+				jQuery(container).find('.vertab-content .panel-collapse.collapse').eq(index).collapse('show');				
+			},1000);
+
+		}	
+	});	
+}
